dfa68f9f8ee56203f86be5d7a6b9be326af5575b
rajlomatech2023 <raj@lomatechnology.com>
2025-07-17 13:30:55 +0700

    removed old files

--- currency-exchange-service/src/main/resources/data.sql
+++
@@ -1,8 +0,0 @@
-insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
-values(1001, 'USD', 'INR', '82', '');
-
-insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
-values(1002, 'GBP', 'INR', '100', '');
-
-insert into currency_exchange(id, currency_from, currency_to, conversion_multiple, environment)
-values(1003, 'AUD', 'INR', '60', '');
