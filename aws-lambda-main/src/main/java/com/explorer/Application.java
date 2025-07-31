03255aec555b980101a6ba428485cabc16080b1f
Princeraj3888 <luckyraj2307@gmail.com>
2025-07-31 23:04:38 +0700

    aws lamdba demo added

---
+++ aws-lambda-main/src/main/java/com/explorer/Application.java
@@ -0,0 +1,19 @@
+package com.explorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Import;
+
+import com.explorer.controller.PingController;
+
+
+@SpringBootApplication
+// We use direct @Import instead of @ComponentScan to speed up cold starts
+// @ComponentScan(basePackages = "org.example.controller")
+@Import({ PingController.class })
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ springboot-aws-lambda/src/main/java/com/explorer/aws/lambda/controller/OrderController.java
@@ -0,0 +1,22 @@
+package com.explorer.aws.lambda.controller;
+
+import com.explorer.aws.lambda.domain.Order;
+import com.explorer.aws.lambda.repository.OrderDao;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class OrderController {
+
+    @Autowired
+    private OrderDao orderRepository;
+
+    @GetMapping("/orders")
+    public List<Order> orderList(){
+        return orderRepository.buildOrders();
+    }
+}
