d255781bb60847c02977029a47bd5b7cd10f716a
rajlomatech2023 <raj@lomatechnology.com>
2025-08-09 14:02:09 +0700

    spring security

---
+++ explore-tech-security/src/main/java/com/explore/tech/ExploreTechSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.explore.tech;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploreTechSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploreTechSecurityApplication.class, args);
+	}
+
+}
--- limits-service/src/main/java/com/xplorer/microservices/limits_service/model/MyTest.java
+++ limits-service/src/main/java/com/xplorer/microservices/limits_service/model/MyTest.java
@@ -8,15 +8,31 @@ import java.util.Date;
 
 public class MyTest {
 
-    public static Date getCurrentDateInGMTMinus4() {
-        // Get current time in America/New_York (handles DST properly)
-        ZonedDateTime nowInTargetZone = ZonedDateTime.now(ZoneId.of("America/New_York"));
-        System.out.println(nowInTargetZone);
-        return Date.from(nowInTargetZone.toInstant());
-    }
-
     public static void main(String[] args) {
-        Date initialRunTime = getCurrentDateInGMTMinus4();
-        System.out.println("Scheduler start time (stored): " + initialRunTime);
+        Date currentDate = new Date();
+
+        // Convert current UTC instant to America/New_York time
+        LocalDateTime localDateTimeInGmt4 = currentDate.toInstant()
+                .atZone(ZoneId.of("America/New_York"))
+                .toLocalDateTime()
+                .withSecond(0)
+                .withNano(0);
+        System.out.println("localDateTimeInGmt4: "+localDateTimeInGmt4);
+
+        // Convert that back to an Instant and then Date
+        Date endTime = Date.from(localDateTimeInGmt4
+                .atZone(ZoneId.of("America/New_York"))
+                .toInstant());
+        System.out.println("endTime: "+endTime);
+
+        ZonedDateTime gmt4DateTime = endTime
+                .toInstant()
+                .atZone(ZoneId.of("America/New_York"));
+        System.out.println("gmt4DateTime: "+gmt4DateTime);
+
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        String formatted = gmt4DateTime.format(formatter);
+        System.out.println("API-compatible GMT-4 date string: " + formatted);
     }
 }
