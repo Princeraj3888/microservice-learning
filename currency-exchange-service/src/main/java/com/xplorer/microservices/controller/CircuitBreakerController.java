6f9e0a3422f81e4d2e45c31b198fd36ac147a5c4
Princeraj3888 <luckyraj2307@gmail.com>
2025-09-18 17:08:24 +0530

    resilience4j learning

--- currency-exchange-service/src/main/java/com/xplorer/microservices/controller/CircuitBreakerController.java
+++ currency-exchange-service/src/main/java/com/xplorer/microservices/controller/CircuitBreakerController.java
@@ -1,5 +1,8 @@
 package com.xplorer.microservices.controller;
 
+import io.github.resilience4j.bulkhead.annotation.Bulkhead;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import io.github.resilience4j.retry.annotation.Retry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,16 +16,29 @@ public class CircuitBreakerController {
 
     private Logger logger = LoggerFactory.getLogger(CircuitBreakerController.class);
     @GetMapping("/sampleApi")
-    @Retry(name = "sample-api", fallbackMethod = "hardCodedMethod")
+    //@Retry(name = "sample-api", fallbackMethod = "hardCodedMethod")
+    //@CircuitBreaker(name ="sample-api", fallbackMethod = "hardCodedMethod")
+    //@RateLimiter(name="default")
+    @Bulkhead(name="sample-api")
     public String sampleApi(){
         logger.info("from sample api");
-        ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-api",
+        /*ResponseEntity<String> forEntity = new RestTemplate().getForEntity("http://localhost:8080/some-dummy-api",
                 String.class);
 
-        return forEntity.getBody();
+        return forEntity.getBody();*/
+
+        return "sample api";
     }
 
     public String hardCodedMethod(Exception e){
         return "fallback-method";
+    }
+
+    @GetMapping("/sampleBulkheadDemo")
+    @Bulkhead(name = "sampleBulkheadDemo", type = Bulkhead.Type.SEMAPHORE)
+    public String processRequest() throws InterruptedException {
+        System.out.println("processing request in: "+ Thread.currentThread().getName());
+        Thread.sleep(3000);
+        return "processed by"+Thread.currentThread().getName();
     }
 }
